2e98d8bb9baf773558b6c05a818a1cf26b9e0354
Ganesh.Jogdand <Ganesh.jogdand@gmail.com>
2016-05-11 13:53:07 -0700

    First Commit

---
+++ src/fngpackage/FirstTestNGFile.java
@@ -0,0 +1,21 @@
+package fngpackage;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+
+public class FirstTestNGFile {
+	public String baseurl = "https://po.sikkasoft.com/beta";
+	public WebDriver driver = new FirefoxDriver();
+	
+  @Test
+  public void verifyHomepageTitle() {
+	  driver.get(baseurl);
+	  String expectedTitle = "Practice Optimizer";
+	  String actualTitle = driver.getTitle();
+	  Assert.assertEquals(actualTitle, expectedTitle);
+		driver.quit();
+  }
+}
